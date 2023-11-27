d560eac0012908a51d152a7de3834ef7a8f69b92
0reoAurelien <129755702+0reoAurelien@users.noreply.github.com>
2023-11-27 10:12:41 +0100

    Q1_TP1infoMaj.c
    Displaying a welcome message and a prompt message

---
+++ scripts/Q1_TP1infoMaj.c
@@ -0,0 +1,45 @@
+
+/*
+QUESTION 1 :
+Affichage d’un message d’accueil, suivi d’un prompt simple
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define INPUT_SIZE 200
+
+
+//function to display a welcome message
+void display_welcome() {
+    const char welcome_message[] = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.\n";
+    write(STDOUT_FILENO, welcome_message, sizeof(welcome_message) - 1);
+}
+
+//function to display a prompt message
+void display_prompt() {
+    const char prompt_message[] = "enseash %% ";
+    write(STDOUT_FILENO, prompt_message, sizeof(prompt_message) - 1);
+}
+
+void enseash_exit() {
+    const char goodbye_message[] = "Au revoir !\n";
+    write(STDOUT_FILENO, goodbye_message, sizeof(goodbye_message) - 1);
+}
+
+
+int main() {
+    char input[INPUT_SIZE];
+
+    display_welcome();
+    while (1) {
+        display_prompt();
+
+        // read user input
+        fgets(input, sizeof(input), stdin);
+    }
+
+    return 0;
+}
